4093aa1a48a8d7328eefef40ed8f9663c754e0bb
Federica Donald <federica.donald@ibm.com>
2023-12-27 20:55:31 +0000

    auth check

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,21 +1,22 @@
+import { useUserContext } from '@/context/AuthContext';
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
-    const isAuthenticated = false;
-  return (
-    <>
-        {isAuthenticated ? (
-            <Navigate to='/' />
-        ): (
-            <>
-                <section className='flex flex-1 justify-center items-center flex-col py-10'>
-                    <Outlet />
-                </section>
-            </>
-        )}
-    </>
-  )
+    const { isAuthenticated } = useUserContext();
+    return (
+        <>
+            {isAuthenticated ? (
+                <Navigate to='/' />
+            ): (
+                <>
+                    <section className='flex flex-1 justify-center items-center py-10 flex-row'>
+                        <Outlet />
+                    </section>
+                </>
+            )}
+        </>
+    )
 }
 
 export default AuthLayout
--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,17 +1,25 @@
 import Footer from '@/components/shared/Footer'
 import Navbar from '@/components/shared/Navbar'
+import { useUserContext } from '@/context/AuthContext'
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
+  const { isAuthenticated } = useUserContext();
   return (
-    <div className='w-full md:flex flex-col'>
-        <Navbar />
-        <section className='flex flex-1 h-full w-full'>
-          <Outlet />
-        </section>
-        <Footer />
-    </div>
+    <>
+      {isAuthenticated ? (
+              <div className='w-full md:flex flex-col'>
+              <Navbar />
+              <section className='flex flex-1 h-full w-full'>
+                <Outlet />
+              </section>
+              <Footer />
+          </div>
+      ) :
+        <Navigate to='/sign-in' />
+      }
+    </>
   )
 }
 
